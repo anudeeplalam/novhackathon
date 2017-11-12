92e81e5c644d8d2e1b6bcaf6e2cbb40ec13afcdb
Anudeep.Lalam <Anudeep.Lalam@veritas.com>
2017-11-12 13:02:17 +0530

    Few changes

--- app.py
+++ app.py
@@ -54,8 +54,8 @@ def getjobinfo(req):
 
 
 def makeWebhookResult(data):
-    description = data.get('description')
-    output = "description is: " + description
+    description = data.get('buildable')
+    output = "buildable is: " + description
 
     print("Response:")
     print(output)
